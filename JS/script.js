a24b32dc191ccb4eac8fb3c6751d068f502bf93d
alep-2000 <alessiopiras056@gmail.com>
2023-05-29 10:23:07 +0200

    Add correction

--- JS/script.js
+++ JS/script.js
@@ -5,7 +5,6 @@
 function createSquareElement(){
     let square= document.createElement('div');
     square.classList.add('square');
-    square.classList.add('invisible');
     return square;
 }
 
@@ -13,26 +12,27 @@ function createSquareElement(){
 const button_element= document.getElementById('button');
 
 
-// RECUPERO L'ELEMENTO CON ID='GRID'
-const grid_element= document.getElementById('grid');
-
-// CICLO FOR
-for(let i=0; i<100; i++){
-    let square = createSquareElement();
-    square.innerText= i + 1;
-
 // EVENT LISTENER
-    button_element.addEventListener('click', function(){
-        square.classList.remove('invisible');
-        square.classList.add('visible');
-    }) 
-
-    square.addEventListener('click', function(){
-        this.classList.toggle('clicked');
-        console.log(this);
-    })
-
-// APPENDO LA GRIGLIA
-    grid_element.append(square);
+button_element.addEventListener('click', function(){
+    // RECUPERO L'ELEMENTO CON ID='GRID'
+    const grid_element= document.getElementById('grid');
     
-}
+    grid_element.innerHTML= '';
+    
+    // CICLO FOR
+    for(let i=0; i<100; i++){
+        let square = createSquareElement();
+        square.innerText= i + 1;
+        
+        
+        square.addEventListener('click', function(){
+            this.classList.toggle('clicked');
+            console.log(this);
+            
+        }) 
+        // APPENDO LA GRIGLIA
+        grid_element.append(square);
+    }
+        
+})
+    
